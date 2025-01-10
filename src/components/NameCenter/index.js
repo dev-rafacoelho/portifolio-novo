08e41a76e4c42d9060307a1d225bd356a0b36738
RafaelCoelho1803 <rcoelho_de_oliveira@hotmail.com>
2025-01-10 19:56:20 -0300

    ✨ feat: adicionei um verificador de letras, e retirar a ultima letra da variavel, Porem no momento nao estou conseguindo imprimir ela ao vivo, deve ser algum erro de uso no react

--- src/components/NameCenter/index.js
+++ src/components/NameCenter/index.js
@@ -1,8 +1,24 @@
+"use client";
+import { useState, useEffect } from "react";
+
 export default function NameCenter() {
+    const [Item, setItem] = useState("Criativo");
+
+    async function delayLoop() {
+        while (Item != "") {
+            console.log({Item});
+            setItem(Item.slice(0,-1)); 
+            await new Promise(resolve => setTimeout(resolve, 5000));
+        }
+    }
+    
+    delayLoop();
     return (
         <section className="h-[75vh] w-full flex flex-col justify-center items-center">
             <p className="text-[7vw] font-bold">RAFAEL COELHO</p>
-            <p className="text-[5vw] font-extralight -mt-[3vh]">Desenvolvedor <span className="text-red-600">Criativo</span></p>
+            <p className="text-[5vw] font-extralight -mt-[3vh]">
+                Desenvolvedor <span className="text-red-600">{Item}</span>
+            </p>
         </section>
     );
 }
